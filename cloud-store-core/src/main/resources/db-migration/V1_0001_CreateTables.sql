d0aa3fb2a753dc4516284a9bb24b39a8f546401f
Maksim Drahun <DRAHUN.MAXIM@standardbank.co.za>
2015-04-13 17:20:37 +0300

    #13 Authentification

--- cloud-store-core/src/main/resources/db-migration/V1_0001_CreateTables.sql
+++ cloud-store-core/src/main/resources/db-migration/V1_0001_CreateTables.sql
@@ -3,14 +3,37 @@ DROP DATABASE CLOUDSTORE;
 CREATE DATABASE CLOUDSTORE;
 USE CLOUDSTORE;
 
-CREATE TABLE image (
-    id BIGINT AUTO_INCREMENT NOT NULL,
-    file_name VARCHAR(255) NOT NULL,
-    content_type VARCHAR(255) NOT NULL,
-    path VARCHAR(255) NOT NULL,
-    alt VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE customer (
+	id BIGINT AUTO_INCREMENT NOT NULL,
+	customer_name VARCHAR(64),
+	password VARCHAR(64),
+	PRIMARY KEY (id)
 ) ENGINE=INNODB;
+
+INSERT INTO customer (id, customer_name, password) VALUES (1, 'admin', 'password');
+INSERT INTO customer (id, customer_name, password) VALUES (2, 'user', 'password');
+
+CREATE TABLE role (
+	id BIGINT AUTO_INCREMENT NOT NULL,
+	role_name VARCHAR(64),
+	PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+INSERT INTO role (id, role_name) VALUES (1, 'admin');
+INSERT INTO role (id, role_name) VALUES (2, 'customer');
+INSERT INTO role (id, role_name) VALUES (3, 'guest');
+
+CREATE TABLE customer_role (
+	customer_id BIGINT NOT NULL,
+	role_id BIGINT NOT NULL,
+	CONSTRAINT FK_CUSTOMER_ROLE FOREIGN KEY (customer_id) REFERENCES customer (id),
+	CONSTRAINT FK_ROLE_CUSTOMER FOREIGN KEY (role_id) REFERENCES role (id)
+) ENGINE=INNODB;
+
+INSERT INTO customer_role (customer_id, role_id) VALUES (1, 1);
+INSERT INTO customer_role (customer_id, role_id) VALUES (2, 2);
+
+
 
 CREATE TABLE product (
 	id BIGINT AUTO_INCREMENT NOT NULL,
@@ -18,9 +41,7 @@ CREATE TABLE product (
 	description VARCHAR(512),
 	full_description VARCHAR(1024),
 	cost NUMERIC(10,2),
-	main_image_id BIGINT,
-	PRIMARY KEY (id),
-	CONSTRAINT FK_PRODUCT_MAIN_IMAGE FOREIGN KEY (main_image_id) REFERENCES image (id)
+	PRIMARY KEY (id)
 ) ENGINE=INNODB;
 
 CREATE TABLE category (
@@ -35,6 +56,14 @@ CREATE TABLE category_product (
 	product_id BIGINT NOT NULL,
 	CONSTRAINT FK_CATEGORY_PRODUCT FOREIGN KEY (category_id) REFERENCES category (id),
 	CONSTRAINT FK_PRODUCT_CATEGORY FOREIGN KEY (product_id) REFERENCES product (id)
+) ENGINE=INNODB;
+
+CREATE TABLE image (
+    id BIGINT AUTO_INCREMENT NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    content_type VARCHAR(255) NOT NULL,
+    path VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
 ) ENGINE=INNODB;
 
 INSERT INTO product (id, title, description, full_description, cost) VALUES (1, 'Latex balloon green', 'Description Latex balloon green', 'Full description Latex balloon green', '1000.00');
@@ -61,5 +90,7 @@ INSERT INTO category_product (category_id, product_id) VALUES (1, 1);
 INSERT INTO category_product (category_id, product_id) VALUES (3, 1);
 INSERT INTO category_product (category_id, product_id) VALUES (5, 4);
 INSERT INTO category_product (category_id, product_id) VALUES (5, 5);
+INSERT INTO category_product (category_id, product_id) VALUES (7, 5);
 
-
+SELECT r.role_name, 'Roles' FROM role r, customer_role cr, customer c WHERE
+            c.customer_name='admin' AND c.ID=cr.customer_id AND cr.role_id=r.ID
